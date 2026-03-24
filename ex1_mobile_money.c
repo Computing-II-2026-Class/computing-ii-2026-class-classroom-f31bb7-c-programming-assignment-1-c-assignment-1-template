aca22f979d8fbe14d05950d28083e0bdffdb25d9
AKELLO ESTHER <akelloesther640@gmail.com>
2026-03-24 16:44:06 +0300

    Completed Assignment 1

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,37 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Your Full Name AKELLO ESTHER OKALANY*/
+/* Registration Number 25/U/BIO/023/GV*/
+/* Student  Number 2500800023 */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+	float Amount,P,T, Fee; 
+	//P = Transaction Fee Percentage
+	//T = Total Amount to be deducted
+	  
+    //The user can choose a Transaction Fee Percentage depending on the range under which their amount falls.
+	printf("Mobile Money Rates:\n 500-2500 0.04\n 2501-5000 0.02\n 5001-15000 0.033\n 15001-30000\n ");
+	printf("30001-45000 0.017\n 45000-60000 0.0125\n Above 60000 0.01\n");
+    
+    printf("Enter Amount to send: ");
+    scanf("%f", &Amount);
+    
+    printf("Transaction Fee Percentage; ");
+    scanf("%f", &P);
+    
+    Fee = Amount * (P/100);
+    
+    printf("Transaction Fee is: %.2f\n", Fee);
+    
+    T = Fee + Amount;
+    printf("Total Amount Deducted is: %.2f\n", T);
+    
+      printf("\n----- TRANSACTION SUMMARY -----\n");
+    printf("Amount Sent: %.2f\n", Amount);
+    printf("Fee: %.2f UGX\n", Fee);
+    printf("Total Deducted: %.2f UGX\n", T);
+    printf("--------------------------------\n");
 
     return 0;
 }
--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Akello Esther Okalany */
+/* Student Number: 2500800023 */
+/* Registration Number: 25/U/BIO/023/GV */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float UC;
+    int C, Bill;
+    //UC is the Units Consumed
+    //C is the Cost Per Unit
+    
+    printf("Enter the Units Consumed: ");
+    scanf("%f", &UC);
+    
+    printf("Enter Cost Per Unit: ");
+    scanf("%d", &C);
+    
+    Bill = UC*C;
+    printf("Total Bill: %.d", Bill);
 
     return 0;
 }
